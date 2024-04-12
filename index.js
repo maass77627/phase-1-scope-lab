dff329a2cdd9f63d363092184bfb74aa9535d894
kelly_maass <kmaass77627@gmail.com>
2024-04-11 20:14:56 -0500

    new

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "john"
+const  leastFavoriteCustomer = "sam"
+
+function upperCaseCustomerName() {
+    return customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+
+function overWriteBestCustomer() {
+    bestCustomer = 'bob'
+}
+
+function changeLeastFavoriteCustomer() {
+     leastFavoriteCustomer = "ben"
+}
